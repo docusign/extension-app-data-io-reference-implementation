4feed91e9be8d824a737b9c036832253dfdc8a0d
yagnik.patel <yagnik.patel@docusign.com>
2025-01-09 13:29:17 -0800

    feat(model): remove explicit typing on datetimes

--- src/services/dataio.service.ts
+++ src/services/dataio.service.ts
@@ -80,7 +80,7 @@ const convertDateToISO8601 = (data: object, typeName: string): void => {
   const dataRecord: Record<string, unknown> = data as Record<string, unknown>;
   for (const key in dataRecord) {
     if (concept.getProperty(key).getType() === 'DateTime') {
-      dataRecord[key] = moment.utc(dataRecord[key] as string, 'DD/MM/YYYY').local().format('YYYY-MM-DDTHH:mm:ss.SSSZ');
+      dataRecord[key] = moment.utc(dataRecord[key] as string).local().format('YYYY-MM-DDTHH:mm:ss.SSSZ');
     }
   }
 }
