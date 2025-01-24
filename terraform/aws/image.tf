915565f82f040504429242b594ec6066fe81e6be
Artem Bondarenko <artem.o.bondarenko@sigma.software>
2025-01-24 17:39:10 +0200

    refactor(terrafrom): Rename variable to better reflect concept

--- terraform/aws/image.tf
+++ terraform/aws/image.tf
@@ -20,7 +20,7 @@ module "image" {
   app_image_name               = local.application_image_name
   app_image_build_context      = local.application_build_context
   app_image_build_dockerfile   = lookup(local.application_build_dockerfile, var.container_tool, null)
-  app_image_build_target_stage = var.application_build_target_stage
+  app_image_build_target_stage = var.application_environment_mode
   app_image_build_paths        = var.application_build_paths
   app_image_build_args         = local.application_build_arguments
   app_image_build_labels       = var.application_build_labels
