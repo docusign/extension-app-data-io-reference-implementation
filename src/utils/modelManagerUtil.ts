658c3ff60b5b11eafe3baf66f5e114f9c4ae84b2
julie.gordon <julie.gordon@docusign.com>
2025-11-11 10:48:08 -0500

    phone and email types

--- src/utils/modelManagerUtil.ts
+++ src/utils/modelManagerUtil.ts
@@ -3,17 +3,31 @@ import path from "path";
 import fs from 'fs';
 
 export class ModelManagerUtil {
+    private static DATA_MODELS_FOLDER: string = '../dataModel/';
+    private static DATA_MODELS_FILES: string[] = [
+        'contact@1.0.0.cto',
+        'model.cto',
+    ];
+
     /**
      * Create a ModelManager instance from a CTO file.
-     * @param {string} ctoFile - the path to the CTO file.
      * @return {ModelManager} a ModelManager instance.
      */
-    public static createModelManagerFromCTO(ctoFile: string): ModelManager {
+    public static createModelManagerFromCTO(): ModelManager {
         // Needed as this is a workaround to skip line locations in generated AST
         // @ts-ignore
-        const modelManager: ModelManager = new ModelManager({ strict: true, skipLocationNodes: true });
-        modelManager.addCTOModel(fs.readFileSync(path.join(__dirname, "../dataModel/model.cto"),'utf8'));
+        const modelManager: ModelManager = new ModelManager({ strict: true,skipLocationNodes: true });
+        
+        this.DATA_MODELS_FILES.forEach((fileName: string) => {
+          modelManager.addCTOModel(
+            fs.readFileSync(
+              path.join(__dirname, `${this.DATA_MODELS_FOLDER}${fileName}`),
+              'utf8'
+            )
+          );
+        });
+    
         modelManager.validateModelFiles();
-        return modelManager
+        return modelManager;
     }
 }
